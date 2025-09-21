2cb69ff296b4db6fc74ad62f5fbd1abd6f72fce8
Rhys Abalon <Rhysjonathanabalon@gmail.com>
2025-09-22 06:37:33 +0800

    PULL TO MAIN

--- src/Components/Pagecomponents/Purokcard.jsx
+++ src/Components/Pagecomponents/Purokcard.jsx
@@ -26,9 +26,9 @@ const Card = ({ title, Icon, href }) => {
       href={href}
       className="w-50 p-4 rounded border-[1px] border-slate-300 relative overflow-hidden group bg-white"
     >
-      <div className="absolute inset-0 bg-gradient-to-r from-violet-600 to-indigo-600 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
+      <div className="absolute inset-0 bg-gradient-to-r from-green-600 to-green-800 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300" />
 
-      <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-400 group-hover:rotate-12 transition-transform duration-300" />
+      <Icon className="absolute z-10 -top-12 -right-12 text-9xl text-slate-100 group-hover:text-violet-500 group-hover:rotate-12 transition-transform duration-300" />
       <Icon className="mb-2 text-2xl text-violet-600 group-hover:text-white transition-colors relative z-10 duration-300" />
       <h3 className="font-medium text-lg text-slate-950 group-hover:text-white relative z-10 duration-300">
         {title}
