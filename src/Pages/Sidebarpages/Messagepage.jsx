26f1164e34bc2b8268d765a84da2515cfcb4b9e5
Rhys Abalon <Rhysjonathanabalon@gmail.com>
2025-09-27 00:21:56 +0800

    ahead onti

--- src/Pages/Sidebarpages/Messagepage.jsx
+++ src/Pages/Sidebarpages/Messagepage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import MessageTable from '../../Components/Pagecomponents/Messagetable'
 
 const Messagepage = () => {
   return (
-    <div className='p-6 bg-gray-50 min-h-screen flex items-center justify-center'>
-      <h1 className='text-8xl font-bold text-gray-800'>
-        Messages
-      </h1>
+    <div className="p-6 bg-gray-100 min-h-screen">
+      {/* Header */}
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold text-gray-800 mb-2">Messages</h1>
+        <p className="text-gray-600">View and manage community messages</p>
+      </div>
+
+      {/* Message Table Component */}
+      <div className="bg-white rounded-2xl shadow p-4">
+        <MessageTable />
+      </div>
     </div>
   )
 }
